39ae6b170f3f7eac191434a61e15b902d3a7aec0
Gray <silicongray@gmail.com>
2022-08-19 14:43:29 +0500

    create, display & get choice buttons with their input

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,92 +1,52 @@
 
-let choice = ['rock', 'paper', 'scissors'];
 
-let playerSelection;
-let computerSelection;
+const startButton = document.querySelector('#start-btn');
 
-let playerWins = 0;
-let computerWins = 0;
+function startGame(e) {
 
-//  console.log(game()) in browser to run game & show winner in the end
-// game();
+    const startButtonParent = document.querySelector('.buttons');
+    // debugger;
+    startButtonParent.removeChild(startButton);
+
+    createChoiceButtons(startButtonParent);
+    getPlayerSelection();
+
+}
+
+function createChoiceButtons(startButtonParent) {
+
+    const btnRock = document.createElement('button');
+    const btnPaper = document.createElement('button');
+    const btnScissors = document.createElement('button');
+    // debugger;
+    btnRock.setAttribute('class', 'choiceButton');
+    btnPaper.setAttribute('class', 'choiceButton');
+    btnScissors.setAttribute('class', 'choiceButton');
+
+    btnRock.setAttribute('id', 'rock');
+    btnPaper.setAttribute('id', 'paper');
+    btnScissors.setAttribute('id', 'scissors');
+
+    btnRock.textContent = 'rock';
+    btnPaper.textContent = 'paper';
+    btnScissors.textContent = 'scissors';
+
+    startButtonParent.appendChild(btnRock);
+    startButtonParent.appendChild(btnPaper);
+    startButtonParent.appendChild(btnScissors);
+
+
+}
+// debugger;
 
 function getPlayerSelection() {
-    while (!choice.includes(playerSelection)) {
-        playerSelection = prompt("Enter your Choice: ", "rock").toLowerCase();
-
-    }
-    return playerSelection;
-}
-
-function capitalizeSelection(Selection) {
-
-    let capitalized = '';
-    for (let i = 0; i < Selection.length; i++) {
-        (i == 0) ? capitalized = Selection[i].toUpperCase() : capitalized += Selection[i];
-    }
-    return capitalized;
-}
-
-function getComputerSelection(array) {
-
-    return array[Math.floor(Math.random() * array.length)];
+    const choiceButtons = document.querySelectorAll('.choiceButton');
+    choiceButtons.forEach(button => {
+        button.addEventListener('click', (e) => {
+            return e.target.id;
+        });
+    });
 }
 
 
-function playRound(computerSelection, playerSelection) {
-
-    // below lines to use for debugging & or play rounds:
-    console.log(".................................................");
-    console.log("computerSelection: " + computerSelection);
-    console.log("playerSelection: " + playerSelection);
-    // debugger;
-
-    if (computerSelection == playerSelection) {
-        console.log(`Match Draw! ${computerSelection} can't beat ${playerSelection}`);
-        return;
-    } else if (computerSelection == 'Rock' && playerSelection == 'Paper') {
-        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
-        playerWins += 1;
-        return;
-    } else if (computerSelection == 'Rock' && playerSelection == 'Scissors') {
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-        computerWins += 1;
-        return;
-    } else if (computerSelection == 'Paper' && playerSelection == 'Rock') {
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-        computerWins += 1;
-        return;
-    } else if (computerSelection == 'Paper' && playerSelection == 'Scissors') {
-        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
-        playerWins += 1;
-        return;
-    } else if (computerSelection == 'Scissors' && playerSelection == 'Rock') {
-        console.log(`You Win! ${playerSelection} beat ${computerSelection}`);
-        playerWins += 1;
-        return;
-    } else if (computerSelection == 'Scissors' && playerSelection == 'Paper') {
-        console.log(`You Lose! ${computerSelection} beat ${playerSelection}`);
-        computerWins += 1;
-        return;
-    }
-
-}
-
-function game() {
-
-    for (let l = 1; l <= 5; l++) {
-
-        playerSelection = getPlayerSelection();
-        playerSelection = capitalizeSelection(playerSelection);
-        computerSelection = getComputerSelection(choice);
-        computerSelection = capitalizeSelection(computerSelection);
-
-        playRound(computerSelection, playerSelection);
-    }
-
-    if (playerWins > computerWins) {
-        return `You Won by ${playerWins} to ${computerWins}`;
-    } else if (playerWins < computerWins) {
-        return `Computer Won by ${computerWins} to ${playerWins}`;
-    } else return `Match Draw by ${computerWins} to ${playerWins}`;
-}
+startButton.addEventListener('click', (e) => startGame(e));
